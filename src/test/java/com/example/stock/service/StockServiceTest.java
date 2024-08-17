63397858a5c99bb43fa6a7ef8b8494087496958e
moner050 <minhyung0237@gmail.com>
2024-08-17 18:39:37 +0900

    Test: ExecutorService 이용해 동시성 문제가 일어나는 테스트 케이스 생성
    레이스 컨디션이 일어나 테스트 실패.

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -36,6 +41,36 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        // 비동기로 실행하는 작업을 단순화 하여 사용할 수 있게 해주는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 다른 Thread 에서 수행중인 작업이 완료될 때 까지 대기할수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                }
+                finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0
+        // 레이스 컨디션이 일어나 0개가 아님.
+        // 레이스 컨디션 : 2개 이상의 쓰레드가 공유 데이터에 엑세스 가능하며, 동시에 변경을 하려고 할 때 발생
+        assertEquals(0, stock.getQuantity());
     }
 
 }

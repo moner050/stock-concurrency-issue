58f82feb1efe18a353740e404d4ebd5d9e2a4de5
moner050 <minhyung0237@gmail.com>
2024-08-17 20:02:26 +0900

    Feat&Test: NamedLock 을 걸어 동시성 문제 해결
    트랜잭션 종료 시, 락 해제 및 세션 관리를 잘 해줘야 하기 때문에 주의가 필요함.

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,8 +15,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-//    @Transactional
-    public synchronized void decrease(Long id, Long quantity) {
+    // 부모의 트랜잭션과 별도로 실행되야 하기 때문에 propagation 변경
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고를 감소시킨 뒤
         // 갱신된 값을 저장
